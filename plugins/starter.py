e29a7f123ef9ffecb2d91d0209da0e18d1c73346
Knut Hühne <knut@k-nut.eu>
2016-04-18 19:45:53 +0200

    fix syntax errors

--- plugins/starter.py
+++ plugins/starter.py
@@ -10,19 +10,25 @@ attachments = []
 typing_sleep = 0
 
 
-SUPPORTED_CITIES = ["Berlin", "Erlangen", "Essen", "Karlsruhe", "Köln", "Leipzig", "Moers", "München", "Neu-Ulm", "Paderborn", "Ulm", "Wuppertal"]
+SUPPORTED_CITIES = ["Berlin", "Erlangen", "Essen",
+                    "Karlsruhe", "Köln", "Leipzig",
+                    "Moers", "München", "Neu-Ulm",
+                    "Paderborn", "Ulm", "Wuppertal"]
 SUPPORTED_CITIES = [city.lower() for city in cities]
 
 location_regex = re.compile("Wo ist Markt in (?P<location>\w+)\?")
+
+
 def process_message(data):
     logging.debug("process_message:data: {}".format(data))
 
     matches = location_regex.match(data['text']).groupdict()
     if matches:
         location = matches.get('location')
-        if location.lower() in SUPPORTED_CITIES
-            outputs.append([data['channel'], "https://wo-ist-markt.de/%s" % location.lower()])
+        if location.lower() in SUPPORTED_CITIES:
+            message = "https://wo-ist-markt.de/%s"
+            outputs.append([data['channel'], message % location.lower()])
         else:
-            outputs.append([data['channel'], "Es gibt keinen Markt in %s. Füge ihn doch hinzu! https://github.com/wo-ist-markt/wo-ist-markt.github.io" % location.lower()])
-
-
+            message = "Es gibt keinen Markt in %s. Füge ihn doch hinzu!"
+            + "https://github.com/wo-ist-markt/wo-ist-markt.github.io"
+            outputs.append([data['channel'], message % location.lower()])
