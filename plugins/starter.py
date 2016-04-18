b862a2879364adb96cb7e69f40fc815b29f844ed
Knut Hühne <knut@k-nut.eu>
2016-04-18 19:49:44 +0200

    fix more things

--- plugins/starter.py
+++ plugins/starter.py
@@ -1,12 +1,9 @@
 # coding=utf-8
 
-import time
 import re
-import random
 import logging
 crontable = []
 outputs = []
-attachments = []
 typing_sleep = 0
 
 
@@ -14,13 +11,14 @@ SUPPORTED_CITIES = ["Berlin", "Erlangen", "Essen",
                     "Karlsruhe", "Köln", "Leipzig",
                     "Moers", "München", "Neu-Ulm",
                     "Paderborn", "Ulm", "Wuppertal"]
-SUPPORTED_CITIES = [city.lower() for city in cities]
+SUPPORTED_CITIES = [city.lower() for city in SUPPORTED_CITIES]
 
-location_regex = re.compile("Wo ist Markt in (?P<location>\w+)\?")
 
 
 def process_message(data):
+    """ Process the incoming message and reply cool things! """
     logging.debug("process_message:data: {}".format(data))
+    location_regex = re.compile(r"Wo ist Markt in (?P<location>\w+)\?")
 
     matches = location_regex.match(data['text']).groupdict()
     if matches:
@@ -29,6 +27,7 @@ def process_message(data):
             message = "https://wo-ist-markt.de/%s"
             outputs.append([data['channel'], message % location.lower()])
         else:
-            message = "Es gibt keinen Markt in %s. Füge ihn doch hinzu!"
-            + "https://github.com/wo-ist-markt/wo-ist-markt.github.io"
+            message = "\n".join(["Es gibt keinen Markt in %s. Füge ihn doch hinzu!",
+                                 "https://github.com/wo-ist-markt/wo-ist-markt.github.io"
+                                ])
             outputs.append([data['channel'], message % location.lower()])
