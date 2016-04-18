58cd89af8847e57fd50638b8166b307d45327a47
Knut Hühne <knut@k-nut.eu>
2016-04-18 19:39:07 +0200

    do smart things

--- plugins/starter.py
+++ plugins/starter.py
@@ -7,59 +7,20 @@ outputs = []
 attachments = []
 typing_sleep = 0
 
-greetings = ['Hi friend!', 'Hello there.', 'Howdy!', 'Wazzzup!!!', 'Hi!', 'Hey.']
-help_text = "{}\n{}\n{}\n{}\n{}\n{}".format(
-    "I will respond to the following messages: ",
-    "`pybot hi` for a random greeting.",
-    "`pybot joke` for a question, typing indicator, then answer style joke.",
-    "`pybot attachment` to see a Slack attachment message.",
-    "`@<your bot's name>` to demonstrate detecting a mention.",
-    "`pybot help` to see this again.")
 
-# regular expression patterns for string matching
-p_bot_hi = re.compile("pybot[\s]*hi")
-p_bot_joke = re.compile("pybot[\s]*joke")
-p_bot_attach = re.compile("pybot[\s]*attachment")
-p_bot_help = re.compile("pybot[\s]*help")
+SUPPORTED_CITIES = ["Berlin", "Erlangen", "Essen", "Karlsruhe", "Köln", "Leipzig", "Moers", "München", "Neu-Ulm", "Paderborn", "Ulm", "Wuppertal"]
+SUPPORTED_CITIES = [city.lower() for city in cities]
 
+location_regex = re.compile("Wo ist Markt in (?P<location>\w+)\?")
 def process_message(data):
     logging.debug("process_message:data: {}".format(data))
 
-    if data['text'] == "Wo ist Markt?":
-        outputs.append([data['channel'], "https://wo-ist-markt.de"])
+    matches = location_regex.match(data['text']).groupdict()
+    if matches:
+        location = matches.get('location')
+        if location.lower() in SUPPORTED_CITIES
+            outputs.append([data['channel'], "https://wo-ist-markt.de/%s" % location.lower()])
+        else:
+            outputs.append([data['channel'], "Es gibt keinen Markt in %s. Füge ihn doch hinzu! https://github.com/wo-ist-markt/wo-ist-markt.github.io" % location.lower()])
 
-    if p_bot_hi.match(data['text']):
-        outputs.append([data['channel'], "{}".format(random.choice(greetings))])
 
-    elif p_bot_joke.match(data['text']):
-        outputs.append([data['channel'], "Why did the python cross the road?"])
-        outputs.append([data['channel'], "__typing__", 5])
-        outputs.append([data['channel'], "To eat the chicken on the other side! :laughing:"])
-
-    elif p_bot_attach.match(data['text']):
-        txt = "Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots."
-        attachments.append([data['channel'], txt, build_demo_attachment(txt)])
-
-    elif p_bot_help.match(data['text']):
-        outputs.append([data['channel'], "{}".format(help_text)])
-
-    elif data['text'].startswith("pybot"):
-        outputs.append([data['channel'], "I'm sorry, I don't know how to: `{}`".format(data['text'])])
-
-    elif data['channel'].startswith("D"):  # direct message channel to the bot
-        outputs.append([data['channel'], "Hello, I'm the BeepBoop python starter bot.\n{}".format(help_text)])
-
-def process_mention(data):
-    logging.debug("process_mention:data: {}".format(data))
-    outputs.append([data['channel'], "You really do care about me. :heart:"])
-
-def build_demo_attachment(txt):
-    return {
-        "pretext" : "We bring bots to life. :sunglasses: :thumbsup:",
-		"title" : "Host, deploy and share your bot in seconds.",
-		"title_link" : "https://beepboophq.com/",
-		"text" : txt,
-		"fallback" : txt,
-		"image_url" : "https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png",
-		"color" : "#7CD197",
-    }
